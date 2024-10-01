d6d003783e86a45b844127d73d7fe72ac1442a62
Rostron10 <daniel.1000@hotmail.co.uk>
2024-10-01 20:04:40 +0100

    Committing all my work

---
+++ Assets/Players.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+
+    private SphereCollider Test;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
