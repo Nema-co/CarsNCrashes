a92fadb2fea9219e45ec5db79370aae1b2f50d2f
Rostron <daniel.1000@hotmail.co.uk>
2024-10-06 21:27:40 +0100

    Various Changes

---
+++ Assets/GameCheckPoints.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCheckPoints : MonoBehaviour
+{
+
+    GlobalVariables var = new GlobalVariables();
+    List<int> PlayerCount = new List<int>();
+    private int MaxPlayer = 8;
+
+
+    private static int CheckPoint = 0;
+    public int CheckPointNumber = 0; //Set in unity front end per object! Don't change
+    private int MissedCheckPoint;
+    private int MaxCheckPoints = 8;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player")) // Ensure the player has the "Player" tag
+        {
+           if (CheckPointNumber == 1 && CheckPoint == 0)
+            {
+                CheckPoint = 1; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 2 && CheckPoint == 1)
+            {
+                CheckPoint = 2; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 3 && CheckPoint == 2)
+            {
+                CheckPoint = 3; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 4 && CheckPoint == 3)
+            {
+                CheckPoint = 4; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 5 && CheckPoint == 4)
+            {
+                CheckPoint = 5; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 6 && CheckPoint == 5)
+            {
+                CheckPoint = 6; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 7 && CheckPoint == 6)
+            {
+                CheckPoint = 7; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+            }
+            else if (CheckPointNumber == 8 && CheckPoint == 7)
+            {
+                CheckPoint = 8; // Increment the counter
+                Debug.Log("Checkpoint reached! Total checkpoints: " + CheckPoint);
+                // GlobalVariables.FinalCheckPointReached = 1;
+                var.PlayerFinished(1);
+            } else if (CheckPoint < 8)
+            {
+                MissedCheckPoint = CheckPoint + 1;
+                Debug.Log("You've missed a check point, please go back to checkpoint " + MissedCheckPoint);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+
+    public int FinalCheckPointReached = 0;
+    public void PlayerFinished(int CheckPoint)
+    {
+        FinalCheckPointReached = CheckPoint;
+        Debug.Log("Game finished " + FinalCheckPointReached);
+    }
+}
