eed86968d0df8740ef24f6b8fd67cca09e6e9cae
danie <danie@DESKTOP-3PMTS3T>
2024-11-18 00:13:20 +0000

    push everything. Pause menu now starting to work

--- Assets/PauseMenuScript.cs
+++ Assets/PauseMenuScript.cs
@@ -6,18 +6,24 @@ using TMPro;
 public class PauseMenuScript : MonoBehaviour
 {
     private static bool isGamePaused = false;
-    private static GameObject PauseMenu, ResumeButtonObj, OptionsButtonObj, QuitButtonObj;
+    public static GameObject PauseMenu, ResumeButtonObj, OptionsButtonObj, QuitButtonObj;
     private Button Resume, Options, Quit;
 
-    public void Start() {
-        PauseMenu = GameObject.Find("PauseMenuPanel");
 
+    public void Awake()
+    {
+        PauseMenu = GameObject.Find("PauseMenuPanel");
+    }
+
+    public void Start() {
+        
+        //PauseMenu.SetActive(false);
         ResumeButtonObj = GameObject.Find("ResumeButton");
         Resume = ResumeButtonObj.GetComponent<Button>();
-        Resume.onClick.AddListener(resumeGame);
+        
 
         if (Resume != null) {
-            
+            Resume.onClick.AddListener(resumeGame);
         } else
         {
             Debug.Log("Resume button is null");
@@ -28,10 +34,8 @@ public class PauseMenuScript : MonoBehaviour
 
         QuitButtonObj = GameObject.Find("QuitButton");
         Quit = QuitButtonObj.GetComponent<Button>();
-        Quit.onClick.AddListener(SceneManagerScript.ExitBackToMainPage);
-
+        Quit.onClick.AddListener(quitGame);
         PauseMenu.SetActive(false);
-       
     }
 
     public void Update() {
@@ -48,17 +52,24 @@ public class PauseMenuScript : MonoBehaviour
     }
 
     public void resumeGame() {
-        Time.timeScale = 1f;
-        isGamePaused = false;
         PauseMenu.SetActive(false);
+        isGamePaused = false;
+        Time.timeScale = 1f;
         Debug.Log("Resume game!");
     }
 
     public void pauseGame() {
-        Time.timeScale = 0f;
-        isGamePaused = true;
         PauseMenu.SetActive(true);
+        isGamePaused = true;
+        Time.timeScale = 0f;
         Debug.Log("Paused game!");
+
+    }
+
+    public void quitGame() {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+        SceneManagerScript.ExitBackToMainPage();
 
     }
 }
